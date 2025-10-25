6d13221d0786b0f86d6f65f3682019f4152bfbb0
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2025-10-25 17:12:35 -0300

    Refactor IMC calculation script to include height in centimeters and enhance output messages

--- exercicio1_imc.rb
+++ exercicio1_imc.rb
@@ -1,17 +1,17 @@
 puts "Informe seu peso em Kg:"
 weight = gets.chomp.to_f
 
-puts "Informe sua altura:"
+puts "Informe sua altura em (cm):"
 height = gets.chomp.to_f
 
-imc = weight / (height ** 2)
+imc = weight / ((height / 100) ** 2)
 
 if imc < 18.5
-  puts "Você está abaixo do peso"
+  puts "Você está abaixo do peso, o seu IMC é de #{imc.round(2)}"
 elsif imc >= 18.5 && imc < 24.9
-  puts "Você está com peso normal"
+  puts "Você está com peso normal, o seu IMC é de #{imc.round(2)}"
 elsif imc >= 25.0 && imc < 29.9
-  puts "Você está com sobrepeso"
+  puts "Você está com sobrepeso, o seu IMC é de #{imc.round(2)}"
 else
-  puts "Você está com obesidade"
+  puts "Você está com obesidade, o seu IMC é de #{imc.round(2)}"
 end
