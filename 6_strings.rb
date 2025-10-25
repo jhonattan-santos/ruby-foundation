9cae2aeaf22a0d6d75b7d467f6876df6ea3858a3
Jhonattan Santos <jhonattanwesleysantos@gmail.com>
2025-10-25 17:32:35 -0300

    Add movie data script with string comparisons and descriptions

---
+++ 6_strings.rb
@@ -0,0 +1,20 @@
+# Dados do Filme
+name = "De volta para o Futuro"
+name2 = "de volta para o futuro"
+
+puts name == name2
+
+description = "O filme trata de uma viagem no tempo"
+description2 = <<Text
+  O filme #{name} é um classico dos anos 80.
+  que trata de uma viagem no tempo.
+  Protagonizado por Michael J. Fox.
+Text
+
+puts description
+
+puts description2
+
+# Tamanho da String
+puts description.length
+puts description.size
